4779b7847f9ba2b6079a8aad7d8f5fe8dd4d44c0
omolevictor97 <omolevictor97@gmail.com>
2024-02-28 16:09:54 +0100

    README file for instruction and how script works

---
+++ README.md
@@ -0,0 +1,4 @@
+> In the ```contact_save.py``` is a class called ```contacts``` with ```add contact``` for adding 
+> your friends into a list, it takes to `parameters` (name, contact), you also have the `view_contact` method to view a list of your friends (added friends)
+
+> The `add_contact` checks if the `name` you want to add is a string datatype, if not the method returns a `False`, the method also checks if the name intended to be added is already in the list, if yes (Y) then asks you want to forcefully add the name, a prompt comes up and if your respond is (`Y`), it then checks if the `contact` is already in the list, and then makes a decision whether to add the friend or not....
---
+++ contact_save.py
@@ -0,0 +1,54 @@
+import pandas as pd
+class contacts:
+
+    def __init__(self):
+        self.list = list()
+        self.dataframe = pd.DataFrame(columns=["Name", "Contact"])
+    
+    def add_contact(self, name, contact):
+        """
+
+        Args:
+            name ([str]): [this is the name of the friends to be added to the list]
+            contact ([int]): [contacts of each friends]
+        """
+        self.name = name
+        self.contact = contact
+
+        if isinstance(self.name, str) and self.name not in self.list:
+            self.list.extend([self.name, self.contact])
+            return self.list
+        elif type(self.name) != str:
+            print(f"Name {self.name} should be string datatype")
+            return False
+        elif self.name in self.list:
+            print(f"You already have a friend with the name {self.name}")
+            force_add = input("Do you have two friends with the same name, enter (Y/N) ")
+            if force_add == "Y" or force_add == "y" and self.contact not in self.list:
+                self.list.extend([self.name, self.contact])
+                print("Your friend list has been updated already")
+                return self.list
+            else:
+                return "We suspect a duplicate data in your list, you have same person with same contact"
+        else:
+            return self.list
+    def view_contact(self):
+        return self.list
+    # def contact_show(self):
+    #     self.contact_df = pd.DataFrame(
+    #         {'Name' : self.name,
+    #         "Contact" : self.contact}, index = [0]
+    #     )
+
+    #     self.concat_df = pd.concat([self.dataframe, self.contact_df], axis=0, ignore_index=True)
+    #     return "DataFrame Created", self.concat_df
+
+
+
+if __name__ == "__main__":
+    diary = contacts()
+    contact_one = diary.add_contact(name="Victor O", contact=234567890)
+    contact_two = diary.add_contact(name="Victor Opeyemi", contact=234567891)
+    contact_three = diary.add_contact(name="Precious Adigun",contact=2347040508748)
+    print(diary.view_contact())
+
